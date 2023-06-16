ec2a2ea61a715308e1e49b3bcd1d1ad56fc0f096
manupuranic <manupuranic@gmail.com>
2023-06-16 17:51:17 +0530

    assignment 13 task done

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ const li = document.querySelector(".items").firstElementChild;
 const msg = document.querySelector(".msg");
 const itemList = document.querySelector(".items");
 
+const del = document.querySelector(".delete");
+
 const submitHandler = (event) => {
   event.preventDefault();
   const name = document.getElementById("name");
   const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
   if (name.value === "" || email.value === "") {
     msg.innerText = "Please enter all the fields";
     msg.classList.add("error");
@@ -18,6 +21,7 @@ const submitHandler = (event) => {
     let userDetails = {
       name: name.value,
       email: email.value,
+      phone: phone.value,
     };
     localStorage.setItem(email.value, JSON.stringify(userDetails));
     msg.innerText = "Successfully logged the values.";
@@ -26,7 +30,20 @@ const submitHandler = (event) => {
     // add the user details in the list.
     let newli = document.createElement("li");
     newli.className = "item";
-    newli.appendChild(document.createTextNode(name.value + " " + email.value));
+    newli.appendChild(
+      document.createTextNode(
+        name.value + " " + email.value + " " + phone.value
+      )
+    );
+
+    // add delete button to the list
+    let delbtn = document.createElement("button");
+    delbtn.className = "delete";
+    delbtn.appendChild(document.createTextNode("Delete"));
+
+    newli.appendChild(delbtn);
+
+    delbtn.addEventListener("click", removeUser);
 
     itemList.appendChild(newli);
 
@@ -35,6 +52,7 @@ const submitHandler = (event) => {
     }, 3000);
     name.value = "";
     email.value = "";
+    phone.value = "";
   }
 };
 
@@ -46,12 +64,19 @@ btn.addEventListener("click", (e) => {
   }, 3000);
 });
 
-li.addEventListener("mouseout", () => {
-  li.style.background = "#f4f4f4";
-  li.style.color = "black";
-});
+// li.addEventListener("mouseout", () => {
+//   li.style.background = "#f4f4f4";
+//   li.style.color = "black";
+// });
 
-li.addEventListener("mouseover", () => {
-  li.style.background = "black";
-  li.style.color = "white";
-});
+// li.addEventListener("mouseover", () => {
+//   li.style.background = "black";
+//   li.style.color = "white";
+// });
+
+const removeUser = (e) => {
+  let email = e.target.parentElement.firstChild.textContent.split(" ")[1];
+  console.log(e);
+  localStorage.removeItem(email);
+  itemList.removeChild(e.target.parentElement);
+};
